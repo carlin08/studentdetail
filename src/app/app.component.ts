22593920e08e7fb5fa11f20299952c398d8c59b4
carlin08 <carlinahana08@gmail.com>
2025-04-11 12:31:00 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StudentdetailComponent } from './studentdetail/studentdetail.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [StudentdetailComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/studentdetail/studentdetail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-studentdetail',
+  imports: [],
+  templateUrl: './studentdetail.component.html',
+  styleUrl: './studentdetail.component.css'
+})
+export class StudentdetailComponent {
+
+}
